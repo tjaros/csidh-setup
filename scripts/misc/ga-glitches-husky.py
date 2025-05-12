48285bd9ae8879475047e88f1d5c03b8ce7b5015
Tomas Jaros <tjaros.822@gmail.com>
2025-05-12 16:34:33 +0200

    misc: remove scripts generated from notebooks

--- scripts/misc/ga-glitches-husky.py
+++ scripts/misc/ga-glitches-husky.py
@@ -10,8 +10,8 @@ from tqdm.notebook import trange
 import random
 import secrets
 
-N_ITERS = 80
-POPSIZE = 200
+N_ITERS = 5
+POPSIZE = 100
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, encoding="utf-8")
@@ -22,7 +22,7 @@ random.seed(seed)
 def algo_search(csidh):
     """Parameter search using own algorithm"""
 
-    population = generate_population_custom(POPSIZE, Unit.EXT_OFFSET_RANGE)
+    population = generate_population(POPSIZE)
     N_scanned = []
 
     logger.info("Starting GA")
@@ -53,11 +53,15 @@ def algo_search(csidh):
 def main():
     logger.setLevel(logging.DEBUG)
     # Path to the CSIDH torget source code
-    PATH = "/home/attacker/Documents/tjaros/git/csidh-target/src/"
+    PATH = "/home/xjaros2/Documents/git/csidh-setup/csidh-target/src/"
     attack_type = "A2"
 
     # Initialize the CSIDH wrapper
     csidh = CSIDH(PATH, attack_type=attack_type)
+
+    csidh.setup()
+
+    csidh.flash_target()
 
     # Capture the first public key
     csidh.scope.arm()
@@ -70,37 +74,24 @@ def main():
     print(f"{EXPECTED_PUBLIC=}")
     print(f"{EXT_OFFSET_MAX=}")
 
-    # Glitch setup
-    csidh.scope.glitch.enabled = True
-    csidh.scope.glitch.clk_src = "pll"
-    csidh.scope.glitch.output = "clock_xor"
-    csidh.scope.glitch.trigger_src = "ext_continuous"
-    csidh.scope.glitch.num_glitches = 1
-    csidh.scope.clock.clkgen_src = "system"
-    csidh.scope.clock.adc_mul = 4
+    csidh.voltage_glitching_setup()
 
-    num_glitches = csidh.scope.glitch.num_glitches
-    csidh.scope.io.hs2 = "glitch"
     Unit.is_husky = True
-
-    # Adc timeout
-    csidh.scope.adc.timeout = 5
-
     # Setup unit parameter ranges
-    Unit.OFFSET_MIN = 1000
-    Unit.OFFSET_MAX = csidh.scope.glitch.phase_shift_steps // 2
+    Unit.OFFSET_MIN = 2500
+    Unit.OFFSET_MAX = 3000
     Unit.OFFSET_RANGE = Unit.OFFSET_MAX - Unit.OFFSET_MIN
 
-    Unit.WIDTH_MIN = 1000
-    Unit.WIDTH_MAX = csidh.scope.glitch.phase_shift_steps // 2
+    Unit.WIDTH_MIN = 2500
+    Unit.WIDTH_MAX = 3000
     Unit.WIDTH_RANGE = Unit.WIDTH_MAX - Unit.WIDTH_MIN
 
     Unit.EXT_OFFSET_MIN = 0
-    Unit.EXT_OFFSET_MAX = EXT_OFFSET_MAX
+    Unit.EXT_OFFSET_MAX =  EXT_OFFSET_MAX
     Unit.EXT_OFFSET_RANGE = Unit.EXT_OFFSET_MAX - Unit.EXT_OFFSET_MIN
 
-    Unit.REPEAT_MIN = 3
-    Unit.REPEAT_MAX = 15
+    Unit.REPEAT_MIN = 4
+    Unit.REPEAT_MAX = 6
     Unit.repeat_range = Unit.REPEAT_MAX - Unit.REPEAT_MIN
 
     print("seed: {}".format(seed))
